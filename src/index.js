df327a23339d96c9a46327fbd4ea44aecb7ed4ef
root <root@Serg167.localdomain>
2019-09-25 01:48:15 +0300

    fix: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 == 1) {
+        return false;
+    } else {
+        //define variables
+        var i = 0;
+        var foundSubArr = true;
+
+        //logic implementation
+        while (i < str.length - 1) {
+            for (j in bracketsConfig) {
+                if ((str[i] == bracketsConfig[j][0]) && (str[i + 1] == bracketsConfig[j][1])) {
+                    str = str.substring(0, i) + str.substring(i + 2, str.length);
+                    (i > 0) && i--;
+                    foundSubArr = true;
+                    break;
+                }
+                foundSubArr = false;
+            }
+            !foundSubArr && i++;
+        }
+
+        //result check
+        if (str.length == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
 }
